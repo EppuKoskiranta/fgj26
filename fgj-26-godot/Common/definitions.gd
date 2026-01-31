3a338c5e891eaa88c6be744df4e8db607f261062
Peter Ahola-Szalay <szalay.peter.1986@gmail.com>
2026-01-31 14:23:39 +0200

    input mapping and key bindings

--- fgj-26-godot/Common/definitions.gd
+++ fgj-26-godot/Common/definitions.gd
@@ -1,11 +1,65 @@
 extends Node
 
 # Add global signals here
+signal input_mapping_changed(new_state : GameInputState)
+signal move_player(move_dir : Vector2)
+signal turn_player(turn_left_deg_per_sec : float)
+signal look_up_down(pitch_angle_deg_per_sec : float)
+signal interact()
+signal menu_toggled()
+signal back_to_previous()
 
 # Add global enums here
+enum GameInputState {MENU, FPS_MOVEMENT, CONVERSATION, LOTION_MIXING, MASK_LOTION_APPLY}
+const INPUT_MAPPING_MENU : GameInputState = GameInputState.MENU
+const INPUT_MAPPING_FPS_MOVEMENT : GameInputState = GameInputState.FPS_MOVEMENT
+const INPUT_MAPPING_CONVERSATION : GameInputState = GameInputState.CONVERSATION
+const INPUT_MAPPING_LOTION_MIXING : GameInputState = GameInputState.LOTION_MIXING
+const INPUT_MAPPING_MASK_LOTION_APPLY : GameInputState = GameInputState.MASK_LOTION_APPLY
 
 # Add global constants and parameters here
+var game_input_state : GameInputState = GameInputState.FPS_MOVEMENT
 
 func _ready() -> void:
 	set_process(false)
 	set_physics_process(false)
+	
+func subscribe_to_movement(move_callback : Callable, turn_callback : Callable, look_callback: Callable) -> void:
+	move_player.connect(move_callback)
+	turn_player.connect(turn_callback)
+	look_up_down.connect(look_callback)
+	
+func subscribe_to_input_mapping_changed(callback : Callable) -> void:
+	input_mapping_changed.connect(callback)
+	
+func subscribe_to_interaction(callback : Callable) -> void:
+	interact.connect(callback)
+	
+func subscribe_to_menu_toggle(callback : Callable) -> void:
+	menu_toggled.connect(callback)
+	
+func subscribe_to_going_back(callback : Callable) -> void:
+	back_to_previous.connect(callback)
+
+func set_input_mapping(new_mapping : GameInputState) -> void:
+	game_input_state = new_mapping
+	emit_signal("input_mapping_changed", new_mapping)
+	
+func set_movement(move_dir : Vector2) -> void:
+	emit_signal("move_player", move_dir)
+	
+func set_turn_speed(turn_left_deg_per_sec : float) -> void:
+	emit_signal("turn_player", turn_left_deg_per_sec)
+	
+func set_look_speed(pitch_angle_deg_per_sec : float) -> void:
+	emit_signal("look_up_down", pitch_angle_deg_per_sec)
+	
+func report_interact() -> void:
+	emit_signal("interact")
+	
+func toggle_menu() -> void:
+	emit_signal("menu_toggled")
+
+func go_back() -> void:
+	emit_signal("back_to_previous")
+	
---
+++ fgj-26-godot/InputHandling/input_handler.gd
@@ -0,0 +1,57 @@
+extends Node
+class_name InputHandler
+
+@export var mouse_sensitivity : float = 1.0
+
+const MOUSE_REL_TO_TURN_DEG_PER_SEC : float = 4.0
+const MOUSE_REL_TO_LOOK_DEG_DELTA : float = 3.0
+const TURN_MIN_DEG_PER_SEC : float = 5.0
+const LOOK_MIN_DEG_PER_SEC : float = 5.0
+
+var is_test : bool = false
+
+func _ready() -> void:
+	set_process(false)
+	set_physics_process(false)
+	Def.subscribe_to_input_mapping_changed(self._input_handling_changed)
+	
+func _unhandled_input(event: InputEvent) -> void:
+	
+	if event.is_action_pressed("move_forward") or event.is_action_pressed("move_backward") \
+		or event.is_action_pressed("move_left") or event.is_action_pressed("move_right") \
+		or event.is_action_released("move_forward") or event.is_action_released("move_backward") \
+		or event.is_action_released("move_left") or event.is_action_released("move_right"):
+		if Def.INPUT_MAPPING_FPS_MOVEMENT == Def.game_input_state:
+			var movement_vector : Vector2 = Vector2(
+				Input.get_axis("move_forward", "move_backward"),
+				Input.get_axis("move_left", "move_right")
+			)
+			Def.set_movement(movement_vector)
+	elif event is InputEventMouseMotion:
+		var relative_mouse_motion: Vector2 = event.relative
+		if Def.INPUT_MAPPING_FPS_MOVEMENT == Def.game_input_state:
+			relative_mouse_motion *= Vector2(MOUSE_REL_TO_TURN_DEG_PER_SEC, MOUSE_REL_TO_LOOK_DEG_DELTA) \
+				* mouse_sensitivity
+			Def.set_turn_speed(-relative_mouse_motion.x if absf(relative_mouse_motion.x) > TURN_MIN_DEG_PER_SEC else 0.0)
+			Def.set_look_speed(-relative_mouse_motion.y if absf(relative_mouse_motion.y) > LOOK_MIN_DEG_PER_SEC else 0.0)
+	elif event.is_action_released("interact"):
+		if Def.game_input_state != Def.INPUT_MAPPING_MENU:
+			Def.report_interact()
+	elif event.is_action_released("open_menu"):
+		Def.toggle_menu()
+	elif event.is_action_released("back"):
+		match Def.game_input_state:
+			Def.INPUT_MAPPING_CONVERSATION, Def.INPUT_MAPPING_LOTION_MIXING, Def.INPUT_MAPPING_MASK_LOTION_APPLY:
+				Def.go_back()
+		
+	elif is_test:
+		return
+	
+	# Do not propagate inputs anymore
+	get_tree().root.set_input_as_handled()
+
+func _input_handling_changed(new_mapping : Def.GameInputState) -> void:
+	if Def.INPUT_MAPPING_FPS_MOVEMENT == new_mapping:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
