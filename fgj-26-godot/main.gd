a4472d8f72f3837942e1f5fcf8bc1200cfac24f6
Peter Ahola-Szalay <szalay.peter.1986@gmail.com>
2026-01-31 15:57:48 +0200

    Basic player and placeholder map

--- fgj-26-godot/main.gd
+++ fgj-26-godot/main.gd
@@ -1,8 +1,13 @@
 extends Node
 
+@onready var player: Player = %Player
+@onready var input_handler: InputHandler = %InputHandler
+@onready var player_camera: PlayerCamera = %PlayerCamera
 
 func _ready() -> void:
-	pass
+	player_camera.init(player.fps_view)
+	# TODO: do it from GameManager
+	Def.set_input_mapping(Def.INPUT_MAPPING_FPS_MOVEMENT)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
