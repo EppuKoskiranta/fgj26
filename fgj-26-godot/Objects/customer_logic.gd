1c5f1bca12a2a7bb9625335d2c6c62f3913f5cea
Peter Ahola-Szalay <szalay.peter.1986@gmail.com>
2026-02-01 13:09:00 +0200

    Customer preferences

--- fgj-26-godot/Objects/customer_logic.gd
+++ fgj-26-godot/Objects/customer_logic.gd
@@ -25,6 +25,22 @@ var player_camera : Node3D = null
 
 var state : State = State.REUSABLE
 var nr_of_asks_left : int = MAX_NR_OF_QUESTIONS
+var preferences : Dictionary[String, int] = {
+	"hydration" : 0,
+	"cleansing" : 0,
+	"anti_aging" : 0,
+	"soothing and calming" : 0,
+	"brightening" : 0
+}
+var adjectives : Dictionary[String, String] = {
+	"hydration" : "dry",
+	"cleansing" : "dirty",
+	"anti_aging" : "old",
+	"soothing_calming" : "irritated",
+	"brightening" : "dull"
+}
+var preference_rate : Array[String] = [" ", "little", "some", "decent", "lot of" ]
+var nr_of_important_properties : int = 0
 
 const MIN_DISTANCE_M : float = 0.8
 const COLLISION_MASK : int = 7
@@ -37,8 +53,30 @@ func _ready() -> void:
 	generate_preferences()
 	
 func generate_preferences() -> void:
-	# TODO: randomize internal variables of customer expectations
+	var nr_of_important_properties = (randi() % 3) + 1
+	var randomized_categories : Array[String] = preferences.keys().duplicate()
+	randomized_categories.shuffle()
+	var random_values : Array[int] = [4]
+	match nr_of_important_properties:
+		2:
+			random_values[0] = (randi() % 3) + 1
+			random_values.push_back(4-random_values[0])
+		3:
+			random_values[0] = 1
+			random_values.push_back(1)
+			random_values.push_back(1)
+			random_values[randi() % 3] = 2
+	
+	for idx in range(nr_of_important_properties):
+		preferences[randomized_categories[idx]] = random_values[idx]
+
 	nr_of_asks_left = MAX_NR_OF_QUESTIONS
+	
+func get_preferences() -> Array[float]:
+	var retval : Array[float] = []
+	for value : int in preferences.values():
+		retval.push_back(float(value) / 4.0)
+	return retval
 
 func move_state() -> void:
 	match state:
@@ -95,13 +133,69 @@ func set_interactability(is_interactable : bool) -> void:
 	interactable.enable(is_interactable)
 	
 func get_text() -> String:
-	self.nr_of_asks_left -= 1
-	# TODO: Gnerate test based on customer expectations
+	var customer_say : String = "I already told you everything you need to know."
+
 	match self.nr_of_asks_left:
+		2:
+			customer_say = _get_casual_answer()
 		1:
-			return "TODO: Polite answer"
-		0:
-			return "TODO: Straight answer"
+			customer_say = _get_straight_answer()
 		_:
-			self.nr_of_asks_left = max(self.nr_of_asks_left, -1)
-			return "I already told you everything you need to know."
+			pass
+	self.nr_of_asks_left -= 1
+	self.nr_of_asks_left = max(self.nr_of_asks_left, 0)
+	return customer_say
+
+func _get_casual_answer() -> String:
+	var possible_answers : Array[String] = []
+	match nr_of_important_properties:
+		1:
+			var preference : String = preferences.find_key(4)
+			var adjective : String = adjectives[preference]
+			possible_answers.push_back("Could you provide an intense %s treatment please? My skin is %s" % [preference, adjective])
+			possible_answers.push_back("Hello there! I need some searious %s! Just look at my face..." % preference)
+		2:
+			var str_format : Array[String] = []
+			for key in preferences.keys():
+				if preferences[key] > 0:
+					str_format.push_back(key)
+					str_format.push_back(preference_rate[preferences[key]])
+					str_format.push_back(adjectives[key])
+			possible_answers.push_back("Good day!\nAm I in the right place for a %s %s and %s %s?" % [str_format[1], str_format[0], str_format[4], str_format[3]])
+			possible_answers.push_back("Please help!\nMy skin is %s and %s.\nI need a %s %s and perhaps %s %s" % [str_format[2], str_format[5], str_format[1], str_format[0], str_format[4], str_format[3]])
+		_:
+			var str_format : Array[String] = []
+			for key in preferences.keys():
+				if preferences[key] > 0:
+					str_format.push_back(key)
+					str_format.push_back(preference_rate[preferences[key]])
+					str_format.push_back(adjectives[key])
+			possible_answers.push_back("Hi there!\nAm I in the right place for %s %s,\n%s %s and %s %s?" % [str_format[1], str_format[0], str_format[4], str_format[3], str_format[7], str_format[6]])
+	return possible_answers.pick_random()
+
+func _get_straight_answer() -> String:
+	var possible_answers : Array[String] = []
+	match nr_of_important_properties:
+		1:
+			var preference : String = preferences.find_key(4)
+			var adjective : String = adjectives[preference]
+			possible_answers.push_back("Like I said, extreme %s" % preference)
+		2:
+			var str_format : Array[String] = []
+			for key in preferences.keys():
+				if preferences[key] > 0:
+					str_format.push_back(key)
+					str_format.push_back(preference_rate[preferences[key]])
+					str_format.push_back(adjectives[key])
+			possible_answers.push_back("I need %s and %s.\nCan you make it quick?" % [str_format[0], str_format[3]])
+			possible_answers.push_back("Like I said,\n%s %s and %s %s" % [str_format[1], str_format[0], str_format[4], str_format[3]])
+		_:
+			var str_format : Array[String] = []
+			for key in preferences.keys():
+				if preferences[key] > 0:
+					str_format.push_back(key)
+					str_format.push_back(preference_rate[preferences[key]])
+					str_format.push_back(adjectives[key])
+			possible_answers.push_back("So, %s, %s and %s %s please" % [str_format[0], str_format[3], str_format[7], str_format[6]])
+	print(preferences, possible_answers)
+	return possible_answers.pick_random()
